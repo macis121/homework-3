f2165d46dc0ac747aed7cce3c548815edfb870da
michelles bells <macis@cca.edu>
2017-02-08 09:18:17 -0800

    Add files via upload

---
+++ sketch-3.3.js
@@ -0,0 +1,27 @@
+Michelle Macis
+exercise 3
+
+function setup() { 
+  createCanvas(400, 400);
+   
+} 
+
+
+
+function draw() {
+ var a=0;
+background (255);
+while( a<=400){
+  
+    
+line(a,0, a, (a*2));
+    
+		a=a+=10;
+   
+   
+  
+   
+    
+  }
+  
+}
